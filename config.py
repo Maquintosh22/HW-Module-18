65d6d82593609db84584a9b7a1ce261e9b79a636
Maquintosh22 <juvefan27@mail.ru>
2023-02-16 13:00:39 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = "6165549771:AAFDKam0M-ubV0m3a_VpD8pqDjH2raJ5B4E"
+
+keys = {'евро': 'EUR',
+        'фунт': 'GBP',
+        'доллар': 'USD',
+        'рубль': 'RUB',
+        'юань': 'CNY'
+
+}
